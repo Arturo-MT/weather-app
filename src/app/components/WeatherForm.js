2decd2e067db8eae1e30e98c7e961528471f8a1a
Brian Torrez <ingatorrez@gmail.com>
2022-02-03 20:02:31 -0600

    Update visual issues

--- src/app/components/WeatherForm.js
+++ src/app/components/WeatherForm.js
@@ -18,8 +18,8 @@ const WeatherForm = props => {
                 <input type= "text" name= "country" placeholder= "Your Country Name" 
                 className= "form-control mt-1" autoComplete="off"/>
             </div>
-            <div className="btn-group mt-1 mx-auto" id="unitSelector" role="group" aria-label="Basic radio toggle button group" onChange={onChangeValue} >
-                <p>Units: </p>
+            <p>Units: </p>
+            <div className="btn-group mt-1 mx-auto" id="unitSelector" role="group" aria-label="Basic radio toggle button group" onChange={onChangeValue} >    
                 <input type="radio" className="btn-check" name="btnradio" id="btnradio1" autoComplete="off" value={"metric"}/>
                 <label className="btn btn-outline-ligth" htmlFor="btnradio1"><b>C°</b></label>
                 <input type="radio" className="btn-check" name="btnradio" id="btnradio2" autoComplete="off" value={"imperial"}/>
